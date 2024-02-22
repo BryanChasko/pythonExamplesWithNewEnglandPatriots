b4f9895c708f166ae319ab88f30a7c7e02372afa
bryanChasko <reddythafox@hotmail.com>
2024-02-22 13:19:08 -0800

    updated player name to use dictionaries player_list['player_name'], indexed with strings wheras our touples and lists were indexed by numbers, i.e. player_list[0].

--- oop_player_getter.py
+++ oop_player_getter.py
@@ -5,15 +5,15 @@ basicConfig(level=INFO, format="%(asctime)s - %(message)s")
 
 def main():
     """ (player_name, university) = get_player() """ # use a comma to create a tuple, similar ot a list but immutable. () add clarity this is a touple.
-    player_tuple = get_player()
-    if player_tuple[0] == "Tom Brady":
-            player_tuple[0] = "🐐 Tom Brady"
-    info(f'{Fore.BLUE}{player_tuple[0]} {Fore.GREEN}went to college at {Fore.RED}{player_tuple[1]}')
+    player_dict = get_player()
+    if "tom" in player_dict["player_name"].lower() or "brady" in player_dict["player_name"].lower():
+        player_dict["player_name"] = "🐐 The GOAT: Tom Brady"
+    info(f'{Fore.BLUE}{player_dict["player_name"]} {Fore.GREEN}went to college at {Fore.RED}{player_dict["university"]}{Fore.RESET}')
 
-def get_player():
-    player_name = input("Enter the player's name: ").strip()
+def get_player():    
+    player_name = input("Enter the player's name: ").strip().lower()
     university = input("Where'd they play college ball? ").strip()
-    return [player_name, university] # adding brackets around the return value makes it a list instead of a tuple.
+    return {"player_name": player_name, "university": university}
 
 if __name__ == "__main__":
     main()
