c6eb0dabc62379c73775e90f2cf6f2a5dd177e1a
bryanChasko <reddythafox@hotmail.com>
2024-02-22 13:57:11 -0800

    most beginner application of using the python class library for object oriented programming.

--- oop_player_getter.py
+++ oop_player_getter.py
@@ -3,17 +3,18 @@ from logging import info, INFO, basicConfig, error
 from colorama import Fore
 basicConfig(level=INFO, format="%(asctime)s - %(message)s")
 
+class Player: 
+    ...  # creating a class called Player
+
 def main():
-    """ (player_name, university) = get_player() """ # use a comma to create a tuple, similar ot a list but immutable. () add clarity this is a touple.
     player_dict = get_player()
-    if "tom" in player_dict["player_name"].lower() or "brady" in player_dict["player_name"].lower():
-        player_dict["player_name"] = "🐐 The GOAT: Tom Brady"
-    info(f'{Fore.BLUE}{player_dict["player_name"]} {Fore.GREEN}went to college at {Fore.RED}{player_dict["university"]}{Fore.RESET}')
+    info(f'{Fore.BLUE}{player_dict.player_name} {Fore.GREEN}went to college at {Fore.RED}{player_dict.university}{Fore.RESET}')
 
 def get_player():    
-    player_name = input("Enter the player's name: ").strip().lower()
-    university = input("Where'd they play college ball? ").strip()
-    return {"player_name": player_name, "university": university}
+    player_dict = Player() # placing a player_dict data type object into our Player class
+    player_dict.player_name =input("What's your favorite patriot player's name? ")
+    player_dict.university = input("Where'd they play college ball? ")
+    return player_dict
 
 if __name__ == "__main__":
     main()
